4ea4006aeb6f6ed92bce4ffed2c4e51b55e01328
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-04-25 18:30:19 +0900

    update searching input

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import * as S from "./style.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   const LogoUrl =
@@ -8,16 +10,41 @@ function Header() {
   const noticeImg =
     "https://media.discordapp.net/attachments/956190154454876183/1100057834709852170/image.png?width=88&height=88";
 
+  const schoolImg =
+    "https://media.discordapp.net/attachments/956190154454876183/1100336868173824030/image.png?width=76&height=76";
+
+  const etcImg =
+    "https://media.discordapp.net/attachments/956190154454876183/1100337621693124658/image.png?width=88&height=88";
+
   return (
     <S.Header>
       <S.HeaderCenter>
         <div className="menu">
-          <S.Logo src={LogoUrl}></S.Logo>
-          <div className="notice">
+          <div className="logoContent">
+            <S.Logo src={LogoUrl}></S.Logo>
+          </div>
+          <div className="notice header">
             <S.noticeImg src={noticeImg} />
             <span>공지</span>
           </div>
+          <div className="school header">
+            <S.schoolImg src={schoolImg} />
+            <span>학교</span>
+          </div>
+          <div className="etc header">
+            <S.etcImg src={etcImg} />
+            <span>기타</span>
+          </div>
         </div>
+        <S.searchContent>
+          <S.searchInput placeholder="search" />
+          <div className="searchIcon">
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="faMagnifyingGlass"
+            />
+          </div>
+        </S.searchContent>
       </S.HeaderCenter>
     </S.Header>
   );
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,36 +1,87 @@
 import styled from "styled-components";
 
 export const Header = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap");
+
   background-color: #007eff;
   width: 100%;
-  height: 70px;
+  height: 60px;
 `;
 
 export const HeaderCenter = styled.div`
-  height: 70px;
+  height: 60px;
   display: flex;
   align-items: center;
 
   .menu {
     display: flex;
+    align-items: center;
   }
 
-  .notice {
+  .header {
     display: flex;
     align-items: center;
+    cursor: pointer;
+    margin: 0 10px 0 10px;
     span {
       color: white;
-      font-weight: 500;
+      margin-left: 5px;
+      font-weight: 400;
+      font-family: "Nanum Gothic", sans-serif;
     }
   }
 `;
 
 export const Logo = styled.img`
-  width: 90px;
-  height: 25px;
+  width: 75px;
+  height: 20px;
+  cursor: pointer;
 `;
 
 export const noticeImg = styled.img`
   width: 20px;
   height: 20px;
+`;
+
+export const searchContent = styled.div`
+  display: flex;
+
+  .searchIcon {
+    display: flex;
+    align-items: center;
+    background-color: white;
+    width: 28px;
+    justify-content: center;
+    border: 1px solid #c0c0c0;
+    border-left: 0;
+
+    .faMagnifyingGlass {
+      color: #636363;
+      font-weight: 500;
+    }
+  }
+`;
+
+export const schoolImg = styled.img`
+  width: 18px;
+  height: 18px;
+  position: relative;
+  bottom: 3px;
+`;
+
+export const etcImg = styled.img`
+  width: 18px;
+  height: 18px;
+`;
+
+export const searchInput = styled.input`
+  width: 150px;
+  height: 30px;
+  outline: none;
+  border: 1px solid #c0c0c0;
+  text-indent: 5px;
+
+  &::placeholder {
+    color: #c0c0c0;
+  }
 `;
