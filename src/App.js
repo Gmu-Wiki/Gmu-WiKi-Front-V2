d44b9216722302616edd75ced3fd951c9a3b87ca
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-04-29 01:04:02 +0900

    setting Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 import React from "react";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/main" element={<Main />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/teacher" element={<Teacher />} />
+        <Route path="/club" element={<Club />} />
+        <Route path="/major" element={<Major />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/notice" element={<Notice />} />
+        <Route path="/*" element={<NotFound />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
