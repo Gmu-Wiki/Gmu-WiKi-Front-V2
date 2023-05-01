86bdb5e23047cd4eb4c1acccbd5cb96657627b37
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-05-01 18:00:48 +0900

    app.js error conflict

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Router>
+      <Routes>
+        {/* \<Route path="/main" element={<C.Main />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/teacher" element={<Teacher />} />
+        <Route path="/club" element={<Club />} />
+        <Route path="/major" element={<Major />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/notice" element={<Notice />} />
+        <Route path="/*" element={<NotFound />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
