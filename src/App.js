8b494107c107e97e586d661c2162806abb249ab9
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-05-01 18:04:14 +0900

    이미지 index.js로 관리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 import React from "react";
 import Header from "./components/Header";
+import * as C from "./pages/index";
 
 function App() {
   return (
