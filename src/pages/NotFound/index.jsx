e85a90bf85498d8a4b7cbd80ad7ceaa59d834910
SeoJumee <official.jumee@gmail.com>
2023-05-06 17:37:01 +0900

    Add Footer component

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import * as I from "../../assets";
 import * as S from "./style";
 
@@ -10,6 +11,7 @@ function NotFound() {
       <S.NotFoundCotainer>
         <I.NotFound />
       </S.NotFoundCotainer>
+      <Footer />
     </>
   );
 }
