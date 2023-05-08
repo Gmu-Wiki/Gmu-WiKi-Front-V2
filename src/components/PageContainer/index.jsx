f39c9575f5f240333c46a5ac174537f034f879d6
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-05-08 23:28:05 +0900

    update PageContainer children props값 설정

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import * as S from "./style";
 import * as C from "../../components";
 
-function PageContainer({ children }) {
+function PageContainer({ children, title, sort }) {
   return (
     <S.PageCenter>
       <S.PageContainer>
-        <C.WhiteContainer title="대문" sort="G무위키" />
+        <C.WhiteContainer title={title} sort={sort} />
         <S.DetailContent>
           <S.DetailCenter>
             <S.Explanation>{children}</S.Explanation>
