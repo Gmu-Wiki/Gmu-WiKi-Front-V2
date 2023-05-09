fb9bcdaceeb3d282c6aa3d68537456d5b53b0a5b
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-05-09 13:51:56 +0900

    props components 지정

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import * as C from "../../components";
-<<<<<<< HEAD
-=======
-import * as S from "../../components/PageContainer/style";
-import PageContainer from "../../components/PageContainer";
->>>>>>> 555656e6b9ded86bc5800b9dd4cd3a728f06ed74
 
 function Main() {
   return (
     <>
       <C.Header />
-<<<<<<< HEAD
-      <C.PageContainer>sd</C.PageContainer>
-=======
-      <PageContainer>니얼굴 이태랑 ㅋ</PageContainer>
->>>>>>> 555656e6b9ded86bc5800b9dd4cd3a728f06ed74
+      <C.PageContainer title="대문" sort="G무위키"></C.PageContainer>
     </>
   );
 }
