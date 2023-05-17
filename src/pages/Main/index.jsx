3b8142012c2e07dd6c79502e8e5b601ab8f50660
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-05-18 00:03:24 +0900

    update G무위키 Illusion

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import * as C from "../../components";
+import * as S from "./style";
 
 function Main() {
   return (
     <>
       <C.Header />
-      <C.PageContainer title="대문" sort="G무위키"></C.PageContainer>
+      <C.PageContainer title="대문" sort="G무위키">
+        <S.Explanation>
+          <S.Title>
+            <span>GSM 학생들이 가꾸어 나가는</span>
+            <span className="point">지식의 나무</span>
+          </S.Title>
+          <S.Illusion>
+            <span className="pointWord">G무위키 </span>
+            <span>에 오신걸 환영합니다.</span>
+          </S.Illusion>
+          <S.Content>
+            <span className="pointWord">G무위키는 </span>
+            <span>GSM 학생들을 위한 위키입니다.</span>
+          </S.Content>
+        </S.Explanation>
+      </C.PageContainer>
     </>
   );
 }
---
+++ src/pages/Main/style.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Explanation = styled.div`
+  height: auto;
+  width: 100%;
+
+  span {
+    color: #636363;
+  }
+
+  .pointWord {
+    color: #007eff;
+  }
+`;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  font-weight: 600;
+  font-size: 22px;
+  margin: 20px 0 10px 0;
+
+  .point {
+    color: #007eff;
+    font-weight: 600;
+    margin-left: 4px;
+  }
+`;
+
+export const Illusion = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 14px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .pointContent {
+    color: #007eff;
+  }
+
+  .contentCenter {
+    display: flex;
+    justify-content: center;
+  }
+`;
