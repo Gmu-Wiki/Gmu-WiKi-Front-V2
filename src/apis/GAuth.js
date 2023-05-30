5656a5a77aaa042a9b0dc73c6d1a9b3857e43bee
한준 <s22073@gsm.hs.kr>
2023-05-30 20:44:06 +0900

    Token을 가져오고 보낼 수 있는 기본 통신 로직 구현

--- src/apis/GAuth.js
+++ src/apis/GAuth.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const EnvConfig = {
   ClientId: process.env.REACT_APP_CLIENT_ID,
@@ -9,10 +11,32 @@ const EnvConfig = {
 
 export default function GauthMove() {
   window.location.href = `https://gauth.co.kr/login?client_id=${EnvConfig.ClientId}&redirect_uri=${EnvConfig.RedirectUri}`;
+  const [searchCode, setSerchCode] = useSearchParams();
+  console.log(searchCode);
+  const code = searchCode.get("code");
+  console.log(code);
+
+  const [storeAccessToken, setStoreAccessToken] = useState("");
+  const [storeRefreshToken, setStoreRefreshToken] = useState("");
+
+  axios
+    .post(EnvConfig.GAuthPostUrl, {
+      code: code,
+      clientId: EnvConfig.ClientId,
+      clientSecret: EnvConfig.ClientSecret,
+      redirectUri: EnvConfig.RedirectUri,
+    })
+    .then(res => {
+      setStoreAccessToken(res.data.accessToken);
+      setStoreRefreshToken(res.data.refreshToken);
+    })
+    .catch(err => console.log(err));
+
+  axios
+    .post("민욱이한테 보낼 URL", {
+      accessToken: storeAccessToken,
+      refreshToken: storeRefreshToken,
+    })
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
 }
-axios.post(EnvConfig.GAuthPostUrl, {
-  code: String,
-  clientId: EnvConfig.ClientId,
-  clientSecret: EnvConfig.ClientSecret,
-  redirectUri: EnvConfig.RedirectUri,
-});
