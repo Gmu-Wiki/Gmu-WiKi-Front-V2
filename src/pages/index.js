e972e437247f86fc7fb15e27fe07eb31c5ec3b66
SeoJumee <official.jumee@gmail.com>
2023-06-08 10:38:12 +0900

    Add page Link 설정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 export { default as Main } from "./Main";
 export { default as NotFound } from "./NotFound";
-// export { default as Club } from "./Club";
-// export { default as Event } from "./Event";
-// export { default as Major } from "./Major";
-// export { default as Notice } from "./Notice";
-// export { default as Teacher } from "./Teacher";
-// export { default as Student } from "./Student";
-export {default as Post} from "./Post";
+export { default as Club } from "./Club";
+export { default as Event } from "./Event";
+export { default as Major } from "./Major";
+export { default as Notice } from "./Notice";
+export { default as Teacher } from "./Teacher";
+export { default as Student } from "./Student";
+export { default as Post } from "./Post";
+export { default as Inquiry } from "./Inquiry";
+export { default as Schedule } from "./Schedule";
