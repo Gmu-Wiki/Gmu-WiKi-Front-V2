c4a6c42b98fb7e804ab9184e7a421d268fa8cfd7
SeoJumee <official.jumee@gmail.com>
2023-06-08 17:04:57 +0900

    Update footer contents

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,22 +5,6 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <S.FooterConainer>
-      <ul>
-        <li>
-          <Link to="/notice">NOTIFICATION</Link>
-        </li>
-        <li>
-          <Link to="/student">SCHOOL</Link>
-        </li>
-        <li>
-          <Link to="/event">ETC</Link>
-        </li>
-        <li>
-          <a href="https://github.com/Team-Ampersand" target="__blank">
-            GITHUB
-          </a>
-        </li>
-      </ul>
       <span>Copyright 2023. &mpersand All rights reserved.</span>
     </S.FooterConainer>
   );
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,26 +1,14 @@
 import styled from "styled-components";
 export const FooterConainer = styled.div`
   width: 100%;
-  height: 60px;
+  height: 80px;
   background-color: #dddddd;
   position: relative;
   bottom: 0;
-  border-top: 1px solid #c0c0c0;
+  color: #636363;
+  font-size: 18px;
   display: flex;
   align-items: center;
-  color: #636363;
-  justify-content: space-between;
-  font-size: 14px;
+  justify-content: center;
   padding: 0 120px 0 120px;
-  ul {
-    display: flex;
-    list-style: none;
-    li {
-      margin-right: 30px;
-      a {
-        text-decoration: none;
-        color: #636363;
-      }
-    }
-  }
 `;
