69d91256a2e6dc820016a427b6d65d28d30d749e
unknown <momlin069@gmail.com>
2023-06-11 18:45:19 +0900

    update category name

--- src/Hooks/useWrite.js
+++ src/Hooks/useWrite.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import EnvConfig from "../apis/EnvConfig";
 
-const useWrite = (category, yearCategory, title, content) => {
+const useWrite = (category, detailCategory, title, content) => {
   const writePost = () => {
     axios
       .post(EnvConfig.WRITEPOSTURL, {
         title: title,
         content: content,
         boardType: category,
-        yearCategory: yearCategory,
+        detailCategory: detailCategory,
       })
       .then(res => toast.success("글이 등록되었습니다!"))
       .catch(err => toast.error("글 등록에 실패하였습니다!"));
