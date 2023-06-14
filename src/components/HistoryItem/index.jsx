1c7085f13b0bda3cee921b0ca00569f305ca3b0c
한준 <s22073@gsm.hs.kr>
2023-06-14 20:41:55 +0900

    key 중복 값 수정 및 기본 스타일

--- src/components/HistoryItem/index.jsx
+++ src/components/HistoryItem/index.jsx
@@ -1,69 +1,18 @@
-import React, { useState } from 'react';
-
-const HistoryItem = () => {
-    const [dummyData, setDummyData] = useState([
-    {
-      id: 1,
-      title: "2023-06-08 [12:10:23]",
-      editor: "이운린",
-    },
-    {
-      id: 2,
-      title: "2023-06-07 [12:10:23]",
-      editor: "한준",
-    },
-    {
-      id: 3,
-      title: "2023-06-06 [12:10:23]",
-      editor: "이아론",
-    },
-    {
-      id: 4,
-      title: "2023-06-05 [12:10:23]",
-      editor: "이힝힝",
-    },
-    {
-      id: 5,
-      title: "2023-06-04 [12:10:23]",
-      editor: "이왕린",
-    },
-    {
-      id: 6,
-      title: "2023-06-03 [12:10:23]",
-      editor: "윙운린",
-    },
-    {
-      id: 7,
-      title: "2023-06-02 [12:10:23]",
-      editor: "이운왕",
-    },
-    {
-      id: 8,
-      title: "2023-06-01 [12:10:23]",
-      editor: "한운린",
-    },
-    {
-      id: 9,
-      title: "2023-06-00 [12:10:23]",
-      editor: "이아린",
-    },
-    {
-      id: 10,
-      title: "2023-06-10 [12:10:23]",
-      editor: "이아론",
-    },
-  ]);
-
-    return (
-        <>
-        {dummyData.map(item => (
-            <React.Fragment key={item.id}>
-              {item.title}
-              {item.editor}
-            </React.Fragment>
-          ))}
-          </>
-    );
+import React from "react";
+import * as S from "./style";
+import { Link } from "react-router-dom";
+const HistoryItem = ({ dummyData }) => {
+    
+  return (
+    <>
+      {dummyData.map((item) => (
+        <S.Container key={item.id}>
+          <Link to="/history"><S.SubTitle>{item.subTitle}</S.SubTitle></Link>
+          {item.editor}
+        </S.Container>
+      ))}
+    </>
+  );
 };
 
 export default HistoryItem;
---
+++ src/components/HistoryItem/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const SubTitle = styled.div`
+  font-size: 1.1rem;
+  color: #007eff;
+`;
