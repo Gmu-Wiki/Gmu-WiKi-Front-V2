7edbb19f19ba8a328cd787e43409988ddd62130f
SeoJumee <official.jumee@gmail.com>
2023-06-15 23:39:11 +0900

    Add inquiry page

---
+++ src/components/InquiryItem/sytle.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InquiryItemWrapper = styled.div`
+  width: 98%;
+  height: 130px;
+  display: flex;
+  flex-direction: column;
+  border-left: 4px solid #ddd;
+  gap: 18px;
+  padding: 10px 18px;
+  line-height: 24px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba(25, 25, 25, 0.04);
+  }
+`;
+
+export const InquiryItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+`;
