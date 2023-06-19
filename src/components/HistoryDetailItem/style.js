a42dbca68daf7f53b663cfba2f45ba126dc012d3
한준 <s22073@gsm.hs.kr>
2023-06-19 17:14:34 +0900

    퍼블리싱 (완)

--- src/components/HistoryDetailItem/style.js
+++ src/components/HistoryDetailItem/style.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
 export const Content = styled.p`
-  display: flex;
   width: 42vw;
-  margin-left: 15px;
+  margin-left: 20px;
   align-items: center;
   flex-direction: column;
   font-size: 1rem;
   color: #191919;
-  line-height: 30px;
+  line-height: 20px;
+
+  a {
+    color: #007eff;
+  }
 `;
