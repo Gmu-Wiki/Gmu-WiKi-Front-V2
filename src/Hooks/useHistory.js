c144ef6ce9ac4b0771f090d46b9043e7833a9d9a
한준 <s22073@gsm.hs.kr>
2023-06-19 23:43:30 +0900

    State설정 및 불러오기

--- src/Hooks/useHistory.js
+++ src/Hooks/useHistory.js
@@ -1,18 +1,26 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import EnvConfig from "../apis/EnvConfig";
 import { toast } from "react-toastify";
 
 const useHistory = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [createDate, setCreateDate] = useState("");
+  const [editDate, setEditDate] = useState("");
+
   useEffect(() => {
     axios
       .get(EnvConfig.GETHISTORY)
       .then(res => {
-        console.log(res.data);
+        setTitle(res.data.title);
+        setContent(res.data.content);
+        setCreateDate(res.data.createdDate);
+        setEditDate(res.data.editedDate);
       })
       .catch(err => toast.error("무언가가 잘못됬다 "));
   }, []);
-  return null;
+  return title, content, createDate, editDate;
 };
 
 export default useHistory;
--- src/components/HistoryDetailItem/index.jsx
+++ src/components/HistoryDetailItem/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import * as S from "./style";
 import * as C from "../index";
+import useHistory from "../../Hooks/useHistory";
 
 const HistoryDetailItem = () => {
+  const { title, content, creatDate, editDate } = useHistory();
   return (
     <>
       <C.Detail hasNumber={false} title={"개요"}>
         <S.Content>
-          어느 새벽, <a>김새미</a>는 목이 말라 물을 뜨러 정수기에 갔다. 물을 뜨는데
-          멀리서 백혜인 언니로 추정되는 사람이 걸어왔다. 김새미는 반가움에
-          엉덩이를 씰룩거리면서 춤을 췄다. 언니가 점점 다가오는데, 김새미는 문득
-          그 사람이 백혜인 언니가 아니라는 사실을 깨달았다. 알고보니 물 뜨러 온
-          박지예 언니였다..
+          어느 새벽, <a>김새미</a>는 목이 말라 물을 뜨러 정수기에 갔다. 물을
+          뜨는데 멀리서 백혜인 언니로 추정되는 사람이 걸어왔다. 김새미는
+          반가움에 엉덩이를 씰룩거리면서 춤을 췄다. 언니가 점점 다가오는데,
+          김새미는 문득 그 사람이 백혜인 언니가 아니라는 사실을 깨달았다.
+          알고보니 물 뜨러 온 박지예 언니였다..
         </S.Content>
       </C.Detail>
 
