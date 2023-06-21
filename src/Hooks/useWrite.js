281ad8b9169162728e950223f4bf5eef07cdeadf
한준 <s22073@gsm.hs.kr>
2023-06-21 20:23:05 +0900

    header 및 body request 추가

--- src/Hooks/useWrite.js
+++ src/Hooks/useWrite.js
@@ -5,12 +5,16 @@ import EnvConfig from "../apis/EnvConfig";
 const useWrite = (category, detailCategory, title, content) => {
   const writePost = () => {
     axios
-      .post(EnvConfig.WRITEPOSTURL, {
-        title: title,
-        content: content,
-        boardType: category,
-        detailCategory: detailCategory,
-      })
+      .post(
+        EnvConfig.WRITEPOSTURL,
+        {
+          title: title,
+          content: content,
+          boardType: category,
+          boardDetailType: detailCategory,
+        },
+        { headers: { Authorization: "accessToken" } }
+      )
       .then(res => toast.success("글이 등록되었습니다!"))
       .catch(err => toast.error("글 등록에 실패하였습니다!"));
   };
