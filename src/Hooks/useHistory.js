4f32833bc4c66518dc67b303dc34f073752d6be5
한준 <s22073@gsm.hs.kr>
2023-06-22 20:38:04 +0900

    try~catch문으로 개선

--- src/Hooks/useHistory.js
+++ src/Hooks/useHistory.js
@@ -10,17 +10,23 @@ const useHistory = () => {
   const [editDate, setEditDate] = useState("");
 
   useEffect(() => {
-    axios
-      .get(EnvConfig.GETHISTORY)
-      .then(res => {
-        setTitle(res.data.title);
-        setContent(res.data.content);
-        setCreateDate(res.data.createdDate);
-        setEditDate(res.data.editedDate);
-      })
-      .catch(err => toast.error("무언가가 잘못됬다 "));
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(EnvConfig.GETHISTORY);
+        const historyData = response.data;
+        setTitle(historyData.title);
+        setContent(historyData.content);
+        setCreateDate(historyData.createdDate);
+        setEditDate(historyData.editedDate);
+      } catch (error) {
+        toast.error("글을 불러오는데 실패했습니다.");
+      }
+    };
+
+    fetchData();
   }, []);
-  return (title, content, createDate, editDate);
+
+  return { title, content, createDate, editDate };
 };
 
 export default useHistory;
