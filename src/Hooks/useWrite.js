5286a5fd417f82b6b44bff3cfdb51d67f38cf359
한준 <s22073@gsm.hs.kr>
2023-06-23 13:42:44 +0900

    header 수정

--- src/Hooks/useWrite.js
+++ src/Hooks/useWrite.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import EnvConfig from "../apis/EnvConfig";
+import useToken from "./useToken";
 
 const useWrite = (category, detailCategory, title, content) => {
-  const writePost = () => {
-    axios
-      .post(
+  const { accessToken } = useToken();
+
+  const writePost = async () => {
+    try {
+      await axios.post(
         EnvConfig.WRITEPOSTURL,
         {
           title: title,
@@ -13,10 +16,12 @@ const useWrite = (category, detailCategory, title, content) => {
           boardType: category,
           boardDetailType: detailCategory,
         },
-        { headers: { Authorization: "accessToken" } }
-      )
-      .then(res => toast.success("글이 등록되었습니다!"))
-      .catch(err => toast.error("글 등록에 실패하였습니다!"));
+        { headers: { Authorization: accessToken } }
+      );
+      toast.success("글이 등록되었습니다!");
+    } catch (error) {
+      toast.error("글 등록에 실패하였습니다!");
+    }
   };
 
   return writePost;
