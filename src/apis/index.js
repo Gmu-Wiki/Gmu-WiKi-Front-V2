9a905e0c42b474dd9416fda098d5ef9eebb36c82
한준 <s22073@gsm.hs.kr>
2023-06-29 10:25:15 +0900

    apis 요청 로직 추가

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,44 @@
 import axios from "axios";
 import EnvConfig from "./EnvConfig";
+import TokenManager from "./TokenManager";
 
 const API = axios.create({
   baseURL: EnvConfig.GMUIKI_SERVER_URL,
   withCredentials: true,
+});
+
+API.interceptors.request.use(async config => {
+  const tokenManager = new TokenManager();
+
+  if (
+    !tokenManager.validateToken(
+      tokenManager.accessExp,
+      tokenManager.accessToken
+    ) &&
+    tokenManager.validateToken(
+      tokenManager.refreshExp,
+      tokenManager.refreshToken
+    )
+  ) {
+    //   await reissueToken()
+    //   tokenManager.initToken()
+  } else if (
+    !tokenManager.validateToken(
+      tokenManager.accessExp,
+      tokenManager.accessToken
+    ) &&
+    !tokenManager.validateToken(
+      tokenManager.refreshExp,
+      tokenManager.refreshToken
+    )
+  )
+    tokenManager.removeTokens();
+
+  config.headers["Authorization"] = tokenManager.accessToken
+    ? `Bearer ${tokenManager.accessToken}`
+    : undefined;
+
+  return config;
 });
 
 export default API;
