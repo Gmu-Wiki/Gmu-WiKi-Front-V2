82e49c67e8d6c5d5112301a94245d117ff1320ee
한준 <s22073@gsm.hs.kr>
2023-07-08 02:08:48 +0900

    공지 글 불러오기 리팩토링

--- src/pages/Notice/index.jsx
+++ src/pages/Notice/index.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import * as C from "../../components";
 import * as S from "./style";
-import API from "../../apis";
 import { useState } from "react";
-import { toast } from "react-toastify";
 import { useEffect } from "react";
-import useNotice from "../../Hooks/useNotice";
+import { useFetch } from "../../Hooks";
 
 export default function Notice() {
   const [noticeList, setNoticeList] = useState([]);
 
-  const getNoticeList = async () => {
-    try {
-      const { data } = await API.get("/admin/notice");
-
+  const { fetch } = useFetch({
+    url: `/admin/notice`,
+    method: "get",
+    onSuccess: data => {
       setNoticeList(data.noticeList);
-    } catch (e) {
-      toast.error("공지 목록을 불러오지 못함.");
+    },
+    errors: {
+      400: "공지 목록을 불러오지 못함."
     }
-  };
+  });
 
   useEffect(() => {
-    getNoticeList();
+    fetch();
   }, []);
 
   return (
