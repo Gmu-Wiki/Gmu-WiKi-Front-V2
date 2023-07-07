11680c010844b7eaa6d51a585611c3298d48ff78
한준 <s22073@gsm.hs.kr>
2023-07-07 11:09:27 +0900

    글 보내기 로직

--- src/Hooks/useUpload.js
+++ src/Hooks/useUpload.js
@@ -2,25 +2,23 @@ import { useCallback } from "react";
 import API from "../apis";
 import { toast } from "react-toastify";
 
-const useUpload = () => {
-  const title = "운린이";
-  const content = "히히";
-  const boardType = "STUDENT";
-  const boardDetailType = "FIFTH";
+const useUpload = ({ props }) => {
+  console.log("useUpload.js", props);
+
   const uploadHandler = useCallback(async () => {
     try {
       await API.post("/user/board", {
-        title: title,
-        content: content,
-        boardType: boardType,
-        boardDetailType: boardDetailType
+        title: props.title,
+        content: props.content,
+        boardType: props.category,
+        boardDetailType: props.detailCategory
       });
 
       toast.success("글  성공");
     } catch (e) {
       toast.error("글 등록 실패");
     }
-  }, []);
+  }, [props.title, props.content, props.category, props.detailCategory]);
 
   return { uploadHandler };
 };
