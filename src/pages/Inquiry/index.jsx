f20d3519732491f2a6538ce4691dca57cc04b1f9
한준 <s22073@gsm.hs.kr>
2023-07-08 02:11:37 +0900

    문의 글 불러오기 리펙토링

--- src/pages/Inquiry/index.jsx
+++ src/pages/Inquiry/index.jsx
@@ -1,29 +1,24 @@
 import React, { useState, useEffect } from "react";
 import * as C from "../../components";
 import * as S from "./style";
-import API from "../../apis";
-import { toast } from "react-toastify";
-import { setUuid } from "../../store/uuid";
-import { useDispatch } from "react-redux";
+import { useFetch } from "../../Hooks";
 
 export default function Inquiry() {
-  const dispatch = useDispatch();
-
   const [inquiryList, setInquiryList] = useState([]);
 
-  const getInquiry = async () => {
-    try {
-      const { data } = await API.get("/admin/inquiry");
-
+  const { fetch } = useFetch({
+    url: `/admin/inquiry`,
+    method: "get",
+    onSuccess: data => {
       setInquiryList(data.inquiryList);
-    } catch (e) {
-      toast.error("문의 정보를 가져오는데 실패함");
-      console.log(e);
+    },
+    errors: {
+      400: "문의 정보를 가져오지 못함"
     }
-  };
+  });
 
   useEffect(() => {
-    getInquiry();
+    fetch();
   }, []);
 
   return (
@@ -32,10 +27,7 @@ export default function Inquiry() {
       <C.Header />
       <C.PageContainer title="문의" sort="문의">
         {inquiryList.map(item => (
-          <C.InquiryItem
-            key={item.uuid}
-            onClick={() => dispatch(setUuid(item.uuid))}
-          >
+          <C.InquiryItem key={item.id}>
             <S.InquiryTitleContainer>
               <S.StyledTitle>{item.title}</S.StyledTitle>
               <S.Sort>{item.inquiryType}</S.Sort>
