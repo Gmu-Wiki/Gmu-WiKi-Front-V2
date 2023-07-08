0f0e193eb400d8d1bbaf8516d7fa640b6bf2ef50
SeoJumee <official.jumee@gmail.com>
2023-07-08 22:57:09 +0900

    Fix 오류 해결

--- src/pages/Role/index.jsx
+++ src/pages/Role/index.jsx
@@ -16,10 +16,8 @@ export default function Role() {
             <S.RoleTitle>이메일</S.RoleTitle>
             <S.RoleInput type="text" />
             <S.RoleTitle>권한</S.RoleTitle>
-            <S.Select name="role">
-              <option value="none" selected>
-                선택해주세요
-              </option>
+            <S.Select name="role" defaultValue="none">
+              <option value="none">선택해주세요</option>
               <option value="admin">관리자</option>
               <option value="user">사용자</option>
             </S.Select>
