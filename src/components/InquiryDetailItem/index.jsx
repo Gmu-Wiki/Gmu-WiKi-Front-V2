0f7b7bac1cfce06ab52050def799ef70411a7d0b
한준 <s22073@gsm.hs.kr>
2023-07-09 16:16:14 +0900

    거부, 승인 버튼 퍼블리싱

--- src/components/InquiryDetailItem/index.jsx
+++ src/components/InquiryDetailItem/index.jsx
@@ -19,23 +19,27 @@ const InquiryDetailItem = ({ id, title, content, name, inquiryType }) => {
   return (
     <>
       <C.Detail hasNumber={false} title={title}>
+        <S.NTBox>
+          <S.Name>작성자 : {name}</S.Name>
+          <S.InquiryType>문의 종류 : {inquiryType}</S.InquiryType>
+        </S.NTBox>
+        <S.BtnBox>
+          <S.ApproveBtn
+            onClick={() => {
+              handleApproveMail();
+            }}
+          >
+            승인
+          </S.ApproveBtn>
+          <S.RefusalBtn
+            onClick={() => {
+              handleRefusalMail();
+            }}
+          >
+            거부
+          </S.RefusalBtn>
+        </S.BtnBox>
         <S.Content>{content}</S.Content>
-        <p>{name}</p>
-        <p>{inquiryType}</p>
-        <button
-          onClick={() => {
-            handleApproveMail();
-          }}
-        >
-          승인
-        </button>
-        <button
-          onClick={() => {
-            handleRefusalMail();
-          }}
-        >
-          거부
-        </button>
       </C.Detail>
       {showRefusal && (
         <C.Refusal
--- src/components/InquiryDetailItem/style.js
+++ src/components/InquiryDetailItem/style.js
@@ -13,3 +13,50 @@ export const Content = styled.p`
     color: #007eff;
   }
 `;
+
+export const NTBox = styled.div`
+  display: flex;
+  margin-left: 70%;
+`;
+
+export const Name = styled.p`
+  font-size: 1rem;
+`;
+
+export const InquiryType = styled.p`
+  font-size: 0.8rem;
+  margin-left: 20px;
+  color: #999;
+`;
+
+export const BtnBox = styled.div`
+  display: flex;
+  margin-left: 84%;
+`;
+
+export const ApproveBtn = styled.button`
+  color: #999;
+  border: 1px solid #999;
+  background: none;
+  width: 60px;
+  height: 30px;
+  cursor: pointer;
+
+  :hover {
+    border: 1px dotted #999;
+  }
+`;
+
+export const RefusalBtn = styled.button`
+  color: #999;
+  border: 1px solid #999;
+  background: none;
+  width: 60px;
+  height: 30px;
+  margin-left: 5px;
+  cursor: pointer;
+
+  :hover {
+    border: 1px dotted #999;
+  }
+`;
