21e656ed30489b5d4702fad4bc7dedeace89e2c6
unknown <momlin069@gmail.com>
2023-07-10 22:46:37 +0900

    update GlobalStyle

--- src/components/WriteBox/PreviewWriteBox/MarkdownConverter/index.jsx
+++ src/components/WriteBox/PreviewWriteBox/MarkdownConverter/index.jsx
@@ -1,15 +1,14 @@
 import useMarkdown from "../../../../Hooks/useMarkdown";
-import * as S from "./style";
 
 function MarkDownConverter({ value }) {
-  const {markdownToHtml} = useMarkdown();
-  
+  const { markdownToHtml } = useMarkdown();
+
   const html = markdownToHtml(value);
-    
+
   return (
-    <S.MarkdownContainer>
+    <>
       <div dangerouslySetInnerHTML={{ __html: html }} />
-   </S.MarkdownContainer>
+    </>
   );
 }
 
