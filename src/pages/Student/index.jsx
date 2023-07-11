85cd454cad21848b0176fd98206298b6e7aa3097
한준 <s22073@gsm.hs.kr>
2023-07-11 10:00:33 +0900

    boardList 5기6기7기 분류

--- src/pages/Student/index.jsx
+++ src/pages/Student/index.jsx
@@ -9,7 +9,15 @@ export default function Student() {
   const data = GetRole();
   const { boardList, roleUrl } = useBoard({ boardType: "STUDENT" });
 
-  if (!boardList) return;
+  if (!boardList) return null;
+
+  console.log(boardList);
+
+  const fifthGen = boardList.filter(item => item.boardDetailType === "FIFTH");
+  const sixthGen = boardList.filter(item => item.boardDetailType === "SIXTH");
+  const seventhGen = boardList.filter(
+    item => item.boardDetailType === "SEVENTH"
+  );
 
   return (
     <>
@@ -18,21 +26,42 @@ export default function Student() {
       <C.PageContainer
         title="학생"
         sort="학생"
-        hasPostButton
-        {...(data === "관리자"
-          ? { hasPostButton: true }
-          : { hasPostButton: false })}
+        hasPostButton={data === "관리자"}
         url="/post"
       >
-        {boardList.map(item => (
-          <React.Fragment key={item.id}>
-            <S.StudentBox>
-              <Link to={`/${roleUrl}/board/${item.id}`}>
-                <S.StudentTitle>{item.title}</S.StudentTitle>
-              </Link>
-            </S.StudentBox>
-          </React.Fragment>
-        ))}
+        <C.Detail hasNumber={false} title={"5기"}>
+          {fifthGen.map(item => (
+            <React.Fragment key={item.id}>
+              <S.StudentBox>
+                <Link to={`/${roleUrl}/board/${item.id}`}>
+                  <S.StudentTitle>{item.title}</S.StudentTitle>
+                </Link>
+              </S.StudentBox>
+            </React.Fragment>
+          ))}
+        </C.Detail>
+        <C.Detail hasNumber={false} title={"6기"}>
+          {sixthGen.map(item => (
+            <React.Fragment key={item.id}>
+              <S.StudentBox>
+                <Link to={`/${roleUrl}/board/${item.id}`}>
+                  <S.StudentTitle>{item.title}</S.StudentTitle>
+                </Link>
+              </S.StudentBox>
+            </React.Fragment>
+          ))}
+        </C.Detail>
+        <C.Detail hasNumber={false} title={"7기"}>
+          {seventhGen.map(item => (
+            <React.Fragment key={item.id}>
+              <S.StudentBox>
+                <Link to={`/${roleUrl}/board/${item.id}`}>
+                  <S.StudentTitle>{item.title}</S.StudentTitle>
+                </Link>
+              </S.StudentBox>
+            </React.Fragment>
+          ))}
+        </C.Detail>
       </C.PageContainer>
 
       <C.ScrollButton />
