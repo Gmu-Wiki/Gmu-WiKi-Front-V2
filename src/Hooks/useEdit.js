014147b4d27b1c3e4774366bdf981d2ac841b97c
한준 <s22073@gsm.hs.kr>
2023-07-14 13:37:33 +0900

    에러 상태코드 추가

--- src/Hooks/useEdit.js
+++ src/Hooks/useEdit.js
@@ -3,6 +3,7 @@ import API from "../apis";
 import { toast } from "react-toastify";
 import GetRole from "../lib/GetRole";
 import { useNavigate } from "react-router-dom";
+import { AxiosError } from "axios";
 
 const useUpload = ({ props }) => {
   const data = GetRole();
@@ -28,7 +29,17 @@ const useUpload = ({ props }) => {
       toast.success("편집 되었습니다.");
       navigate("/");
     } catch (e) {
-      toast.error("편집에 실패하였습니다.");
+      if (!(e instanceof AxiosError)) {
+        toast.error("편집에 실패하였습니다.");
+        return;
+      }
+      if (e.response && e.response.status >= 500) {
+        toast.error("500");
+      } else if (e.response && e.response.status >= 409) {
+        toast.error("이미 존재하는 제목입니다.");
+      } else if (e.response && e.response.status >= 400) {
+        toast.error("변경 내용이 없습니다.");
+      }
     }
   }, [props, roleUrl, navigate]);
 
@@ -42,7 +53,14 @@ const useUpload = ({ props }) => {
       toast.success("편집 되었습니다.");
       navigate("/");
     } catch (e) {
-      toast.error("편집에 실패하였습니다.");
+      if (!(e instanceof AxiosError)) {
+        toast.error("이미 존재하는 제목입니다.");
+        console.log(e);
+        return;
+      }
+      if (e.response && e.response.status >= 409) {
+        toast.error("이미 존재하는 제목입니다.");
+      }
     }
   }, [props, roleUrl, navigate]);
 
