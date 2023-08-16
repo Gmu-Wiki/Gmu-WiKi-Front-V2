08eb1cf92df149512689773f9becce49ecdd60c1
한준 <s22073@gsm.hs.kr>
2023-08-17 08:37:34 +0900

    Update postbox font color

--- src/components/WriteBox/EditWriteBox/style.js
+++ src/components/WriteBox/EditWriteBox/style.js
@@ -10,13 +10,13 @@ export const EditWriteBoxContainer = styled.div`
     font-size: 1rem;
     font-weight: 500;
   }
-`
+`;
 
 export const CategoryInputBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: ${(props) => props.type === '문의목적' ? '14.8vw' : '26vw'}; 
+  width: ${props => (props.type === "문의목적" ? "14.8vw" : "26vw")};
   margin-bottom: 10px;
 
   select {
@@ -27,7 +27,7 @@ export const CategoryInputBox = styled.div`
     color: #999999;
     font-weight: 300;
   }
-`
+`;
 
 export const TitleInputBox = styled.div`
   display: flex;
@@ -36,20 +36,19 @@ export const TitleInputBox = styled.div`
   width: 60vw;
   margin-bottom: 10px;
 
-
   span {
     margin-left: 12px;
   }
-  
+
   input {
     border: 1px solid #dddddd;
     width: 55.2vw;
     height: 25px;
     padding-left: 8px;
     outline: none;
-    color: #999999;
+    color: #191919;
   }
-`
+`;
 
 export const ContentInputBox = styled.div`
   display: flex;
@@ -61,7 +60,7 @@ export const ContentInputBox = styled.div`
     width: 55.2vw;
     height: auto;
     padding: 8px;
-    color: #999999;
+    color: #191919;
     border: 1px solid #dddddd;
     font-size: 0.8rem;
     outline: none;
@@ -69,7 +68,7 @@ export const ContentInputBox = styled.div`
     line-height: 22px;
     overflow-y: hidden;
   }
-`
+`;
 
 export const LineNumberBox = styled.div`
   display: flex;
@@ -77,4 +76,4 @@ export const LineNumberBox = styled.div`
   align-items: center;
   line-height: 22px;
   margin-left: 20px;
-`
+`;
