fb778d85e3791c4109ef212774b54113b8ed0bbc
SeoJumee <official.jumee@gmail.com>
2023-08-18 09:12:19 +0900

    Add vercel analytics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Router />
         <ToastContainer />
       </Provider>
+      <Analytics />
     </>
   );
 }
