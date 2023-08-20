26cc3c0664c752a39314e231c3a37f668ddfbc40
한준 <s22073@gsm.hs.kr>
2023-08-21 01:48:16 +0900

    FIx history margin

--- src/components/HistoryDetailItem/style.js
+++ src/components/HistoryDetailItem/style.js
@@ -1,13 +1,10 @@
 import styled from "styled-components";
 
 export const Content = styled.p`
-  width: 42vw;
-  margin-left: 20px;
-  align-items: center;
-  flex-direction: column;
   font-size: 1rem;
   color: #191919;
   line-height: 26px;
+  margin-top: -40px;
 
   a {
     color: #007eff;
